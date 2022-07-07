6ae631042d6b9bfffbba3cd4a8997cb35f097be2
Eno1993 <46813592+Eno1993@users.noreply.github.com>
2022-07-07 10:08:37 +0900

    Time: 109 ms (5.00%), Space: 43.3 MB (87.28%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        int npoint =0;
+        int mpoint =0;
+        
+        while(true){
+            if(npoint==nums1.length&&mpoint<nums2.length){
+                for(; mpoint<nums2.length; mpoint++){
+                    list.add(nums2[mpoint]);
+                }
+            }else if(npoint<nums1.length&&mpoint==nums2.length){
+                for(; npoint<nums1.length; npoint++){
+                    list.add(nums1[npoint]);
+                }
+            }else if(npoint<nums1.length&&mpoint<nums2.length){
+                if(nums1[npoint]==nums2[mpoint]){
+                    list.add(nums1[npoint]);
+                    list.add(nums2[mpoint]);
+                    npoint++;
+                    mpoint++;
+                }else if(nums1[npoint]<nums2[mpoint]){
+                    list.add(nums1[npoint]);
+                    npoint++;
+                }else{
+                    list.add(nums2[mpoint]);
+                    mpoint++;
+                }
+            }else{
+                break;
+            }
+        }
+        for(int a : list){
+            System.out.print(a +" ");
+        }
+        
+        int size = list.size();
+        if(size%2==0){//짝수
+            return (double)(list.get((size/2)-1)+list.get(size/2))/2;
+        }else{//홀수
+            return (double)list.get(size/2);
+        }
+    }
+}
